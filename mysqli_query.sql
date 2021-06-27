1973950dbf2422309c1b2e38dd60b65178023d27
weaamalsarani <86329897+weaamalsarani@users.noreply.github.com>
2021-06-27 09:53:11 +0300

    Create mysqli_query.sql

---
+++ mysqli_query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `mysql`.`mysqli_query` ( `id` INT NOT NULL AUTO_INCREMENT ,
+ `forward` VARCHAR(255) NOT NULL , 
+ `left` VARCHAR(255) NOT NULL ,
+ `stop` VARCHAR(255) NOT NULL ,
+ `right` VARCHAR(255) NOT NULL , 
+ `backward` VARCHAR(255) NOT NULL ,  
+ PRIMARY KEY (`id`)) ENGINE = InnoDB;
